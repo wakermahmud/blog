a1141e137b13e5b04283e63393a92e0377079c2c
Bryan Powell <bryan@metabahn.com>
2016-07-26 15:27:13 -0700

    Manage sitemap along with post state

--- lib/pakyow/blog/schema.rb
+++ lib/pakyow/blog/schema.rb
@@ -13,6 +13,11 @@ Pakyow::Console.data :post, icon: 'newspaper-o' do
     post.published = true
     post.published_at = Time.now unless post.published_at
     post.save
+    
+    Pakyow::Console.sitemap.url(
+      location: File.join(Pakyow::Config.app.uri, post.slug),
+      modified: post.updated_at.httpdate
+    )
   end
 
   action :unpublish,
@@ -21,11 +26,42 @@ Pakyow::Console.data :post, icon: 'newspaper-o' do
          display: ->(post) { post.published? } do |post|
     post.published = false
     post.save
+    
+    Pakyow::Console.sitemap.delete_location(
+      File.join(Pakyow::Config.app.uri, post.slug)
+    )
   end
 
   action :delete, label: 'Delete' do |post|
     post.destroy
     notify("#{post.title} post deleted", :success)
+
+    Pakyow::Console.sitemap.delete_location(
+      File.join(Pakyow::Config.app.uri, post.slug)
+    )
+
     redirect router.group(:data).path(:show, data_id: params[:data_id])
+  end
+end
+
+Pakyow::Console.after :post, :create do |post|
+  if post.published?
+    Pakyow::Console.sitemap.url(
+      location: File.join(Pakyow::Config.app.uri, post.slug),
+      modified: post.updated_at.httpdate
+    )
+  end
+end
+
+Pakyow::Console.after :post, :update do |post|
+  if post.published?
+    Pakyow::Console.sitemap.delete_location(
+      File.join(Pakyow::Config.app.uri, post.initial_value(:slug))
+    )
+    
+    Pakyow::Console.sitemap.url(
+      location: File.join(Pakyow::Config.app.uri, post.slug),
+      modified: post.updated_at.httpdate
+    )
   end
 end
